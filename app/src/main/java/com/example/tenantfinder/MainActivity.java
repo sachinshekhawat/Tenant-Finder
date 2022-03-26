008ccfc7ff61846833e77384089c46a7cca34bf5
Sachin Shekhawat <98947255+sachinshekhawat@users.noreply.github.com>
2022-03-26 16:02:53 +0530

    login and firebase authentication

--- app/src/main/java/com/example/tenantfinder/MainActivity.java
+++ app/src/main/java/com/example/tenantfinder/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.tenantfinder;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
 
     @Override
@@ -16,19 +20,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {
             }
         });
 
-        findViewById(R.id.textviewsignup).setOnClickListener(this);
+       findViewById(R.id.textviewsignup).setOnClickListener(this);
+
+
     }
 
        @Override
             public void onClick(View view) {
                 if (view.getId() == R.id.textviewsignup)
                     startActivity(new Intent(this, RegisterActivity.class));
-
-            }
+             }
         }
