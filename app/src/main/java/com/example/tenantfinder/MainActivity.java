2bb0769cb89ca7cae70115b2929b6444ed6407e8
Sachin Shekhawat <98947255+sachinshekhawat@users.noreply.github.com>
2022-03-29 23:26:21 +0530

    firebase authentication

--- app/src/main/java/com/example/tenantfinder/MainActivity.java
+++ app/src/main/java/com/example/tenantfinder/MainActivity.java
@@ -1,18 +1,31 @@
 package com.example.tenantfinder;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
-
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    TextView textViewforgot;
+    EditText etEmail, etPassword;
+    ProgressBar progressBar;
+    FirebaseAuth mAuth;
 
 
     @Override
@@ -21,20 +34,128 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
 
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+
+
+        etEmail = findViewById(R.id.email);
+        etPassword = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progressbar);
+        textViewforgot = findViewById(R.id.forgot_password);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        textViewforgot.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {
+            public void onClick(View view) {
+
+                EditText resetMail = new EditText(view.getContext());
+                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(view.getContext());
+                passwordResetDialog.setTitle("Reset Password ?");
+                passwordResetDialog.setMessage("Enter your Email to get reset link.");
+                passwordResetDialog.setView(resetMail);
+
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //extract email and send reset link
+
+                        String mail = resetMail.getText().toString();
+                        mAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(MainActivity.this, "Reset link Sent to your email", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, "Error occured ! Link not sent"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    }
+                });
+
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //close dialog
+                    }
+                });
+
+                passwordResetDialog.create().show();
             }
         });
 
-       findViewById(R.id.textviewsignup).setOnClickListener(this);
 
-
+        findViewById(R.id.textviewsignup).setOnClickListener(this);
+        findViewById(R.id.btn_login).setOnClickListener(this);
     }
+        private void userLogin () {
 
-       @Override
-            public void onClick(View view) {
-                if (view.getId() == R.id.textviewsignup)
-                    startActivity(new Intent(this, RegisterActivity.class));
-             }
+            String email = etEmail.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
+
+            if (email.isEmpty()) {
+                etEmail.setError("Email is required");
+                etEmail.requestFocus();
+                return;
+            }
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                etEmail.setError("Please enter a valid Email");
+                etEmail.requestFocus();
+                return;
+            }
+
+
+            if (password.isEmpty()) {
+                etPassword.setError("Password is required");
+                etPassword.requestFocus();
+                return;
+            }
+
+            if (password.length() < 6) {
+                etPassword.setError("Minimum length of password should be 6");
+                etPassword.requestFocus();
+                return;
+            }
+
+            progressBar.setVisibility(View.VISIBLE);
+
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    progressBar.setVisibility(View.GONE);
+                    if (task.isSuccessful()) {
+                        finish();
+                        Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+
+                    } else {
+                        Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
         }
+
+        @Override
+        protected void onStart () {
+            super.onStart();
+            if (mAuth.getCurrentUser() != null) {
+                finish();
+                startActivity(new Intent(this, ProfileActivity.class));
+            }
+        }
+
+
+        @Override
+        public void onClick (View view){
+            switch (view.getId()) {
+                case R.id.textviewsignup:
+                    startActivity(new Intent(this, RegisterActivity.class));
+                    break;
+                case R.id.btn_login:
+                    userLogin();
+            }
+        }
+    }
--- app/src/main/java/com/example/tenantfinder/RegisterActivity.java
+++ app/src/main/java/com/example/tenantfinder/RegisterActivity.java
@@ -1,17 +1,25 @@
 package com.example.tenantfinder;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText etEmail, etPassword;
+    ProgressBar progressBar;
 
     private FirebaseAuth mAuth;
 
@@ -20,49 +28,67 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+
+
         etEmail = findViewById(R.id.email);
         etPassword = findViewById(R.id.password);
+        progressBar = (ProgressBar)findViewById(R.id.progressbar);
 
         mAuth = FirebaseAuth.getInstance();
 
         findViewById(R.id.btn_register).setOnClickListener(this);
+        findViewById(R.id.textview_login).setOnClickListener(this);
     }
 
-    private void registerUser(){
+    private void registerUser() {
         String email = etEmail.getText().toString().trim();
         String password = etPassword.getText().toString().trim();
 
-        if(email.isEmpty()){
+        if (email.isEmpty()) {
             etEmail.setError("Email is required");
             etEmail.requestFocus();
             return;
         }
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             etEmail.setError("Please enter a valid Email");
             etEmail.requestFocus();
             return;
         }
 
 
-        if (password.isEmpty()){
+        if (password.isEmpty()) {
             etPassword.setError("Password is required");
             etPassword.requestFocus();
             return;
         }
 
-        if (password.length()<6){
+        if (password.length() < 6) {
             etPassword.setError("Minimum length of password should be 6");
             etPassword.requestFocus();
             return;
         }
 
-        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
-            if(task.isSuccessful()){
-                Toast.makeText(getApplicationContext(),"User Registered Successfully", Toast.LENGTH_SHORT).show();
-            }
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            progressBar.setVisibility(View.GONE);
+       if (task.isSuccessful()){
+           finish();
+           startActivity(new Intent(RegisterActivity.this,ProfileActivity.class));
+
+       }else{
+
+           if(task.getException() instanceof FirebaseAuthUserCollisionException){
+               Toast.makeText(getApplicationContext(), "This email is already registered", Toast.LENGTH_SHORT).show();
+           }else {
+               Toast.makeText(getApplicationContext(), Objects.requireNonNull(task.getException()).getMessage(),Toast.LENGTH_SHORT).show();
+           }
+       }
         });
     }
 
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
         switch(view.getId()){
@@ -72,6 +98,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                 break;
 
             case R.id.textview_login:
+                finish();
                 startActivity(new Intent(this,MainActivity.class));
                 break;
 
